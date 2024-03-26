bdc67e3451c1bcf21d62075d9230a2788bfc2d14
Haroldo Kimura <140283624+HaroldoDEV1@users.noreply.github.com>
2024-03-25 23:05:39 -0300

    Update fila.js

--- IA.js
+++ IA.js
@@ -1,24 +1,42 @@
-const pergunta = "Qual país você mora?"
+// 1º a entrar é 1º a sair
 
-// Verificando se é uma pergunta
-if(pergunta.endsWith("?")){
+const alunos = [
+    "Vitor",
+    "Volpe",
+    "Kimura",
+    "Gustavo",
+    "Lê",
+]
 
- // Separando as palavras por espaço
-  const palavras = pergunta.split(" ");
 
-  // Obtendo a localização do dispositivo
-  const localizacao = ("Brasil");
+// Criar fila --> criarFila(tamanho = int):any[] --> retorna qualquer coisa
+export function criarFila(tamanho = 5){
+    console.log("Fila criada")
+    return [... new Array(tamanho)]
+}   
+criarFila()
 
-  // Se a localização estiver disponível
-  if(localizacao){
 
-    // Respondendo à pergunta com base na localização
-console.log(`Você mora em ${localizacao}.`);
-
-  } else {
-
-    console.log(`não sei onde voce mora`);
-  }
+// Inserir na fila --> inserirFila(fila:any[], item):void --> passa dois parâmetros:
+// fila que é uma array de qualquer coisa; e retorna vazio
+export function inserirNaFila(fila, item){ 
+    const posicao = fila.indexOf(undefined)
+        if (posicao === -1) { // Se não tiver espaço na fila
+            console.log("Não tem espaço")
+            return
+        }
+    fila[posicao] = item 
+    return
 }
+inserirNaFila()
 
-// Embelezar resposta
+// Retirar da fila --> remover(fila:any[]):any[] --> passa um parâmetro:
+// fila que é uma array de qualquer coisa; e retorna qualquer coisa
+export function removerDaFila(fila){ 
+    if(fila[0] === undefined){
+        console.log("Fila Vazia")
+        return
+    }
+    fila.pop([0])
+}
+removerDaFila()
